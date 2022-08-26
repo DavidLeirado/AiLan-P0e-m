0fb9f021f4854310fc55ad04a55a8ce44127cb56
David Leirado <dleiradomaroto@gmail.com>
2022-08-26 23:08:01 +0200

    feat!: interface for crawler

--- crawler_scripts/crawler.py
+++ crawler_scripts/crawler.py
@@ -4,10 +4,31 @@
 #
 # It will hold the main class and the execution flux to be imported by the top-level main.py
 
-class PoetSpider:
-    """
-    This class will manage the execution flux in order to scrap all the poetry from the web:
-    poesi.as
-    """
-    def run(self):
-        pass
+import os
+import sys
+
+sys.path.append(os.path.abspath("./"))
+
+from crawler_scripts.scrapers import Poem, ScrapPoemsURLs, ScrapAuthors
+
+
+def run_author():
+    ScrapAuthors()
+
+
+def run_poems():
+    Poem()
+
+
+def run_poem_url():
+    ScrapPoemsURLs()
+
+
+def run():
+    run_author()
+    run_poem_url()
+    run_poems()
+
+
+if __name__ == "__main__":
+    run_poem_url()

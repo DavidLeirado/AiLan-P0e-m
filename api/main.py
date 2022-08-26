0e0bf23f80d3c76f47d2df2709e9390e2f963353
David Leirado Maroto <dleiradomaroto@gmail.com>
2022-08-27 01:35:56 +0200

    feat!: logger added to the api

--- api/main.py
+++ api/main.py
@@ -1,14 +1,24 @@
-from fastapi import FastAPI
+import os
+import sys
+
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
 
 from model.poem_generator import PoemGenerator
 
+sys.path.append(os.path.abspath("../"))
+
+from utils.logger import Logger
+
+Logger.info("Loading model . . .")
 pgen = PoemGenerator("./model/spanish_poems_model.pt")
 
+Logger.info("Initializing app . . .")
 app = FastAPI()
 
 
 class PoemRequest(BaseModel):
+    name: str
     text: str
     entry_count: int
     entry_length: int
@@ -22,8 +32,8 @@ class PoemResponse(BaseModel):
 
 
 @app.post("/")
-async def poem_maker(poem_params: PoemRequest) -> PoemResponse:
-
+async def poem_maker(request: Request, poem_params: PoemRequest) -> PoemResponse:
+    Logger.info(f"IP: {request.client.host} - Name: {poem_params.name}")
     poem = pgen.generate(poem_params.text, entry_count=poem_params.entry_count, entry_length=poem_params.entry_length,
                          temperature=poem_params.temperature, top_p=poem_params.top_p)
 

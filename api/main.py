6ce7bb0e5f1ca0b90fc9ff1851eebcbd4be8b986
David Leirado Maroto <dleiradomaroto@gmail.com>
2022-08-26 23:41:05 +0200

    feat: torch loading model

--- api/main.py
+++ api/main.py
@@ -1,8 +1,11 @@
+import torch
 from fastapi import FastAPI
+
+model = torch.load("./model/spanish_poems_model.pt").to("cpu")
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
+@app.post("/")
+async def poem_maker():
     return {"message": "Hello World"}
